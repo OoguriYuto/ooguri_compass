d1d87c6b07eda24d86094e37256f438cc1ec536f
ooguri <ooguri.yuto@compass-japan.com>
2025-03-10 14:55:27 +0900

    改行追加＆usersテーブル修正（mailのUNIQUE化とカラムの追加）

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,3 +1,8 @@
-INSERT INTO users (id, name, mail) VALUES (1, 'A', 'aaaa@mail.com');
-INSERT INTO users (id, name, mail) VALUES (2, 'B', 'bbbb@mail.com');
-INSERT INTO users (id, name, mail) VALUES (3, 'C', 'cccc@mail.com');
+INSERT INTO users (id, name, mail, password, roles, created, last_logined, enabled) 
+VALUES (1, 'A', 'aaaa@mail.com', '1111', 'PM', NOW(), NOW(), 0);
+
+INSERT INTO users (id, name, mail, password, roles, created, last_logined, enabled) 
+VALUES (2, 'B', 'bbbb@mail.com', '2222', 'PL', NOW(), NOW(), 0);
+
+INSERT INTO users (id, name, mail, password, roles, created, last_logined, enabled) 
+VALUES (3, 'C', 'cccc@mail.com', '3333', 'M', NOW(), NOW(), 1);
